212ef070cd981dca2b440f46c646d0556f294f87
-sadmin <sam@masteckit.com>
2018-11-29 23:35:50 +0000

    Trigger my first build on TeamCity

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorldApp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello Universe!");
+			Console.WriteLine("Hello Universe This is Sam The Man!");
+            Console.WriteLine("This change will trigger a build on TeamCity!");
 		}
 	}
 }
